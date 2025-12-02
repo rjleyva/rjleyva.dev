b4fb2e1d3d2cd9dea17a9c74d07355a674fd4172
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-02 02:40:07 -0500

    feat(home): render RecentPost component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import type React from 'react'
 import Hero from '@/components/ui/Hero/Hero'
+import RecentPost from '@/components/ui/RecentPost/RecentPost'
 
 const App = (): React.JSX.Element => {
   return (
     <section id="home-content">
       <Hero heroTagline="Welcome to my blog! I'm RJ, and here I document my web development insights through writing." />
+      <RecentPost />
     </section>
   )
 }
